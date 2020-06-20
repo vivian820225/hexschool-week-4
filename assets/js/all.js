42567a541905bb85421fe6677f2bdce93bb99221
vivian <vivian820225@gmail.com>
2020-06-20 11:54:08 +0800

    Update 2020-06-20T03:53:39.684Z

---
+++ assets/js/all.js
@@ -0,0 +1,13 @@
+"use strict";
+
+$(document).ready(function () {
+  // 主選單收合
+  $('.menu_btn').on('click', function (e) {
+    $('.menu_wrapper').toggleClass('open');
+  }); // FQA
+
+  $('.question').on('click', function (e) {
+    $(this).siblings('.answer').slideToggle();
+  });
+});
+//# sourceMappingURL=all.js.map
